b4b423c4f76ce33f727fc61b30c51dbf92dcac4a
ColinZeDev <hollo1234567890e@gmail.com>
2025-10-29 16:35:45 -0600

    added 404 page, cdn, and other stuff

--- assets/js/getcodingyears.js
+++
@@ -1,9 +0,0 @@
-function gethowlongago(year) {
-    const currentYear = new Date().getFullYear();
-    return currentYear - parseInt(year, 10);
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    const i = document.getElementById('coding-years-ago');
-    i.innerHTML = `(I have been coding for over <b>${gethowlongago('2021')}</b> years)`;
-});
--- assets/js/openemail.js
+++ assets/js/openemail.js
@@ -6,4 +6,4 @@ function copyToClipboard(text) {
     });
 }
 
-function email(){let inp = prompt('Open in email client or copy email?:\n[1] - Open in mail (mailto link)\n[2] - Copy');if (parseInt(inp) === 1){window.open('mailto:cyb3r.colin@gmail.com').focus();}else if (parseInt(inp === 2)){copyToClipboard('cyb3r.colin@gmail.com');}}
+function email(){let inp = prompt('Open in email client or copy email?:\n[1] - Open in mail (mailto link)\n[2] - Copy');if (parseInt(inp) === 1){window.open('mailto:work@cybercolin.org').focus();}else if (parseInt(inp === 2)){copyToClipboard('work@cybercolin.org');}}
